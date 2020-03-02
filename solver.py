0e978d8b91120bf0c8207468f2a428723b795ecb
bandydos <alexandertack@hotmail.be>
2020-03-02 14:41:21 +0100

    Finished

--- solver.py
+++ solver.py
@@ -11,6 +11,46 @@ board = [
 ]
 
 
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+    return False
+
+
+def valid(bo, num, pos):
+    # Check row.
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check col.
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box.
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+    return True
+
+
 # Board visualisation.
 def print_board(bo):
     for i in range(len(bo)):  # For board length.
@@ -27,12 +67,16 @@ def print_board(bo):
                 print(str(bo[i][j]) + " ", end="")
 
 
-print_board(board)  # Call print_board.
-
-
 # Find empty spots (search for 0s).
 def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:  # If number at pos is 0.
                 return i, j  # Return row and col.
+    return None
+
+
+print_board(board)
+print("_______________________\n")
+solve(board)
+print_board(board)
